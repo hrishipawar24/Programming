f63d78ea8e6acb1ec320547f236de78d2e9f164d
Hrishikesh Pawar <70955531+hrishipawar24@users.noreply.github.com>
2024-11-26 21:48:53 +0530

    Update Constructor1.cpp

--- 19/Constructor1.cpp
+++ 19/Constructor1.cpp
@@ -1,38 +1,46 @@
 #include<iostream>
 using namespace std;
 
-class Marvellous
-{
-    public:
-        int No1;
-        int No2;
+// Declare a class named 'Marvellous'
+class Marvellous {
+public:
+    // Declare two integer data members to store values
+    int No1;
+    int No2;
 
-        Marvellous()
-        {
-            cout<<"Inside default constructor\n";
-            No1 = 0;
-            No2 = 0;
-        }
-        Marvellous(int A, int B)
-        {
-            cout<<"Inside parametrised constructor\n";
-            No1 = A;
-            No2 = B;
-        }
-        ~Marvellous()
-        {
-            cout<<"Inside destructor\n";
-        }
-        void Fun()
-        {
-            cout<<"Inside Fun\n";
-        }
+    // Default constructor
+    Marvellous() {
+        cout << "Inside default constructor\n";
+        // Initialize data members to zero
+        No1 = 0;
+        No2 = 0;
+    }
+
+    // Parameterized constructor
+    Marvellous(int A, int B) {
+        cout << "Inside parameterized constructor\n";
+        // Initialize data members with provided values
+        No1 = A;
+        No2 = B;
+    }
+
+    // Destructor
+    ~Marvellous() {
+        cout << "Inside destructor\n";
+    }
+
+    // Member function to print a message
+    void Fun() {
+        cout << "Inside Fun\n";
+    }
 };
 
-int main()
-{
+int main() {
+    // Create an object 'mobj1' using the default constructor
     Marvellous mobj1;
-    Marvellous mobj2(11,21);
+
+    // Create an object 'mobj2' using the parameterized constructor
+    Marvellous mobj2(11, 21);
 
     return 0;
 }
